88a4296d075db8487b179540cc18352c44de1290
Amit <info.amitp@gmail.com>
2023-03-09 23:20:01 +0530

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div style={{backgroundColor:"red"}}>
+        
+<img src="Web capture_9-3-2023_224431_.jpeg" alt="hhi" />
+<div class="jumbotron">
+  <h1 class="display-4">Heavy Haul Institute</h1>
+  <p class="lead">The Heavy Haul Railway Institute in Noida, India is a prestigious organization dedicated to research, development, and training in the field of heavy haul transportation. Founded in the year 2010, the institute has emerged as a leader in providing specialized education and training to professionals in the railway industry.
+
+The Heavy Haul Railway Institute is equipped with state-of-the-art facilities and cutting-edge technologies to support research and development activities related to heavy haul railway transportation. The institute has a team of highly qualified and experienced professionals who work towards developing innovative solutions and technologies for the railway industry.
+
+The primary objective of the institute is to provide specialized training to professionals in the railway industry to enhance their knowledge and skills in the field of heavy haul transportation. The institute offers various programs and courses related to railway engineering, operations, and management, designed to cater to the specific needs of the railway industry.
+
+The Heavy Haul Railway Institute is located in Noida, a well-connected city in the National Capital Region (NCR) of India. The institute's strategic location makes it easily accessible to professionals from different parts of the country and overseas.
+
+Overall, the Heavy Haul Railway Institute in Noida is a world-class institution dedicated to advancing the field of heavy haul railway transportation through research, development, and specialized education and training programs.</p>
+  <hr class="my-4"/>
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+      
+    </div>
+  )
+}
+
+export default Header
